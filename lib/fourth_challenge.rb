c333e4c8e22eb7c803b12e1419ae028c3896466c
Dani Schuhman <dani8439@aol.com>
2018-09-10 17:36:11 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -16,7 +16,8 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
-        { name: , :age , :attitude }
+        { name: , :age , :attitude },
+        { name: , :age , :attitude },
       ]
    }
 }
